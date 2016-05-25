a8bf93c8f48373bda7b6a9473964dd24e0dd16fc
Jignesh  Thummar <jignesh.thummar@bigdecisions.com>
2016-05-26 01:53:40 +0530

    Implement UI rendering from map object from days to person names in sorted order.
    	- Add function to calculate person square width, height based on number of persons.
    	- Add function to create dom node for person.

--- js/main.js
+++ js/main.js
@@ -3,11 +3,12 @@
 
 	var inputYear = document.getElementsByClassName('js-year')[0],
 		btnUpdate = document.getElementsByClassName('js-update')[0],
-		inputYearValue /*= inputYear.value*/,
+		elmCalender = document.getElementsByClassName('js-calendar')[0],
+		inputYearValue,
 		arrDays = [ 'sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat' ];
 
 	function getJsonInput() {
-		return eval(document.getElementById('json-input').innerHTML);
+		return eval(document.getElementById('json-input').value);
 	}
 
 	btnUpdate.onclick = function() {
@@ -26,9 +27,12 @@
 		}); ;
 
 	 	fillSortedDataForDays(inputData, inputYearValue, mapDayToNames); 
+		updateView(mapDayToNames);
 	};
 
 	function fillSortedDataForDays(data, inputYearValue, mapDayToNames) {
+		// TODO: Optimize for subsequent calculations after calculated for 1 year.
+
 		var personData = JSON.parse( JSON.stringify( data ));
 		personData.forEach( function(person) {
 			var arrDate = person.birthday.split('/'),
@@ -47,8 +51,51 @@
 		personData.forEach( function( person ) {
 			mapDayToNames[person.day].push( person.name );
 		});
+	}
+
+	function updateView( mapDayToNames ) {
+
+		arrDays.forEach( function( day) {
+
+			var arrPersons = mapDayToNames[day],
+				cntPersons = arrPersons.length,
+				elmLiCalendarBox = elmCalender.querySelector('[data-day=' + day +']'),
+				elmDaysContainer = elmLiCalendarBox.getElementsByClassName('day__people')[0],
+				elmLiCalendarBoxClassName = elmLiCalendarBox.className;
+
+			elmDaysContainer.innerHTML = '';
+
+
+			if( cntPersons === 0 ) {
+				elmLiCalendarBox.className = elmLiCalendarBoxClassName + ' day--empty';
+			}
+			else {
+				var	width, height;
+				width = height = getWidthFromCount(cntPersons);
+				elmLiCalendarBox.className = elmLiCalendarBoxClassName.replace('day--empty', '');
+
+				arrPersons.forEach( function( person ) {
+					elmDaysContainer.appendChild( createPersonNode(person, width, height));
+				});
+			}
+
+		});
 
 
 		console.log(mapDayToNames);
-	}
+	};
+
+	function getWidthFromCount( count ) {
+		return 100/Math.ceil( Math.sqrt(count))
+	};
+
+	function createPersonNode( name, width, height ) {
+		var elmPerson = document.createElement('div');
+		elmPerson.className = 'day__person';
+		elmPerson.innerText = name;
+		elmPerson.style.width = width + '%';
+		elmPerson.style.height = height + '%';
+
+		return elmPerson;
+	};
 })();
